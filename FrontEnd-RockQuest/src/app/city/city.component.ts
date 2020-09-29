70cbb2f21c4068715f3ea6f741b322e1d747fb56
FreddyPatarroyo <frpatarroyo@poligran.edu.co>
2020-09-29 11:19:21 -0500

    reinicio del font para aplicar meterial

--- FrontEnd-RockQuest/src/app/city/city.component.ts
+++ FrontEnd-RockQuest/src/app/city/city.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { from } from 'rxjs';
-import { SelectItem } from 'primeng/api';
-import {CityService} from './../services/service-city/city.service'
 
 @Component({
   selector: 'app-city',
@@ -10,36 +7,7 @@ import {CityService} from './../services/service-city/city.service'
 })
 export class CityComponent implements OnInit {
 
-  selectedCountries1: string[] = [];
-
-  items: SelectItem[];
-
-  item: string;
-
-  countries: any[];
-
-  constructor(private cityService: CityService) {
-      
-      this.items = [];
-
-      this.cityService.getCountries().then(countries => {
-          this.items = countries;
-      });
-
-      this.countries = [
-          {name: 'Australia', code: 'AU'},
-          {name: 'Brazil', code: 'BR'},
-          {name: 'China', code: 'CN'},
-          {name: 'Egypt', code: 'EG'},
-          {name: 'France', code: 'FR'},
-          {name: 'Germany', code: 'DE'},
-          {name: 'India', code: 'IN'},
-          {name: 'Japan', code: 'JP'},
-          {name: 'Spain', code: 'ES'},
-          {name: 'United States', code: 'US'}
-      ];
-
-  }
+  constructor() {}
 
   ngOnInit(): void {
   }
--- FrontEnd-RockQuest/src/app/services/service-city/city.service.ts
+++ FrontEnd-RockQuest/src/app/services/service-city/city.service.ts
@@ -1,23 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-
 
 @Injectable({
   providedIn: 'root'
 })
 export class CityService {
 
-  constructor(private http: HttpClient) { }
-
-    getCountries() {
-    return this.http.get<any>('assets/countries.json')
-      .toPromise()
-      .then(res => <any[]>res.data)
-      .then(data => { this.getSelectedCountries(data); return data; });
-    }
-
-    getSelectedCountries(cities:any){
-      console.log(cities);
-    }
-    
+  constructor() { }
 }

599f3f4afdb781705e4c3ac36cd32b36f290ddf5
FreddyPatarroyo <frpatarroyo@poligran.edu.co>
2020-09-29 19:01:34 -0500

    adelanto registro

--- FrontEnd-RockQuest/src/app/app.module.ts
+++ FrontEnd-RockQuest/src/app/app.module.ts
@@ -20,6 +20,17 @@ import { AchievementComponent } from './achievement/achievement.component';
 /*_________________________________________________________MATERIAL_________________________________________________________*/
 
 import { MatSliderModule } from '@angular/material/slider';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+
 
 @NgModule({
   declarations: [
@@ -41,8 +52,15 @@ import { MatSliderModule } from '@angular/material/slider';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSliderModule
-
+    MatSliderModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatChipsModule,
+    MatIconModule,
+    MatButtonModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- FrontEnd-RockQuest/src/app/registry/registry.component.ts
+++ FrontEnd-RockQuest/src/app/registry/registry.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-registry',
@@ -6,7 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registry.component.css']
 })
 export class RegistryComponent implements OnInit {
+  hide = true;
 
+  email = new FormControl('', [Validators.required, Validators.email]);
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'llenar este componente';
+    }
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+/*___________________________________________________________________________________________ */
   constructor() { }
 
   ngOnInit(): void {

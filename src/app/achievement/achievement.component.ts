403e5d7c3cedf1144718c1d19b28c19ce71d863c
FreddyPatarroyo <frpatarroyo@poligran.edu.co>
2020-10-10 13:45:08 -0500

    formulario de logros primera entrega

--- src/app/achievement/achievement.component.ts
+++ src/app/achievement/achievement.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators}from '@angular/forms'
 
 @Component({
   selector: 'app-achievement',
@@ -6,6 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./achievement.component.css']
 })
 export class AchievementComponent implements OnInit {
+
+  title = new FormControl('', [Validators.required]);
+  nombre = new FormControl('', [Validators.required]);
+  imagen = new FormControl('', [Validators.required]);
+
+  getErrorMessageTitulo() {
+    return this.title.hasError('required') ? 'componente obligatorio' : '';
+  }
+
+  getErrorMessageNombre() {
+    return this.nombre.hasError('required') ? 'componente obligatorio' : '';
+  }
+
+  getErrorMessageImagen() {
+    return this.imagen.hasError('required') ? 'componente obligatorio' : '';
+  }
 
   constructor() { }
 

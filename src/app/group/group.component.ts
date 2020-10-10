d90754487eff9e55c89df06613c178b42bb1bfe1
FreddyPatarroyo <frpatarroyo@poligran.edu.co>
2020-10-10 13:45:57 -0500

    formulario de grupos primera entrega

--- src/app/group/group.component.ts
+++ src/app/group/group.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-group',
@@ -6,6 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./group.component.css']
 })
 export class GroupComponent implements OnInit {
+
+  title = new FormControl('', [Validators.required]);
+
+  usuario = new FormControl('', [Validators.required]);
+  
+  programa = new FormControl('', [Validators.required]);
+
+  getErrorMessageTitulo() {
+    return this.title.hasError('required') ? 'componente obligatorio' : '';
+  }
+
+  getErrorMessageUsuario() {
+    return this.usuario.hasError('required') ? 'componente obligatorio' : '';
+  }
+
+  getErrorMessagePrograma() {
+    return this.programa.hasError('required') ? 'selecionar un componente' : '';
+  }
+  /*_____________________________________________________________________________________________________________ */
 
   constructor() { }
 

d74201ca85a7d3f345c8d7eb104f92140671c42e
NicolasSoler <josolerh@poligran.edu.co>
2020-11-19 14:56:08 -0500

    Guardar personajes

--- src/app/creador/menu-creador/creador-personaje/imformacion-personaje/imformacion-personaje.component.ts
+++ src/app/creador/menu-creador/creador-personaje/imformacion-personaje/imformacion-personaje.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NpcService } from 'src/app/services/npc.service';
+import { NpcModel } from 'src/model/npc.model';
 
 @Component({
   selector: 'app-imformacion-personaje',
@@ -7,11 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ImformacionPersonajeComponent implements OnInit {
 
-  listaImgPersonajes:string[]=['personaje 1','personaje 2','personaje 3','personaje 4'];
+  listaImgPersonajes;
+  existente;
+  nombre: string;
 
-  constructor() { }
+  constructor(private NPCService:NpcService) {
+    this.updateBase();
+   }
 
   ngOnInit(): void {
+  }
+
+  save(){
+    if(!this.existente){
+      let newNPC = new NpcModel();
+      newNPC.image = "";
+      newNPC.name = this.nombre; 
+      this.NPCService.post(newNPC). subscribe (
+        res =>{
+          console.log(res);
+        }
+      );
+      this.updateBase();
+    }
+  }
+
+  updateBase(){
+    this.listaImgPersonajes = []
+
+    let data = this.NPCService.getAll();
+    data.forEach(element => {
+      this.listaImgPersonajes.push(element);
+    });
+    console.log(this.listaImgPersonajes);
   }
 
 }
--- src/app/services/npc.service.ts
+++ src/app/services/npc.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { NpcModel } from 'src/model/npc.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NpcService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getAll():Observable<NpcModel[]>{
+    return this.http.get <NpcModel[]>(`${environment.url}/npc`)
+  }
+
+  post(item: NpcModel){
+    return this.http.post (`${environment.url}/npc`,item)
+  }
 }
